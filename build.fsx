770f70ef3f23c74c0cfe82358789f6a2e0a10a8f
Robert Jeppesen <robert.jeppesen@durius.se>
2015-06-09 10:15:45 +0200

    Indent 4 everywhere

--- build.fsx
+++ build.fsx
@@ -12,30 +12,30 @@ open Fake.ProcessHelper
 let releaseRepo = "https://github.com/guillermooo/sublime-fsharp-package-releases.git"
 
 // build parameters
-let dataDir     = getBuildParam "sublimeDir"
+let dataDir = getBuildParam "sublimeDir"
 
 ProcessHelper.killCreatedProcesses <- false
 
 let isWindows = (Path.DirectorySeparatorChar = '\\')
 let sublimeDataPath () =
-  let UnixPaths =
-      [  (Environment.GetEnvironmentVariable("HOME") + "/Library/Application Support/Sublime Text 3")
-         (Environment.GetEnvironmentVariable("HOME") + "/.config/sublime-text-3") ]
+    let UnixPaths =
+        [  (Environment.GetEnvironmentVariable("HOME") + "/Library/Application Support/Sublime Text 3")
+           (Environment.GetEnvironmentVariable("HOME") + "/.config/sublime-text-3") ]
 
-  let WindowsPaths =
-    // Non-standard variable. It saves time while developing.
-    [  Environment.GetEnvironmentVariable("SUBLIME_TEXT_DATA")
-       Environment.ExpandEnvironmentVariables(@"%APPDATA%\Sublime Text 3") ]
+    let WindowsPaths =
+      // Non-standard variable. It saves time while developing.
+      [  Environment.GetEnvironmentVariable("SUBLIME_TEXT_DATA")
+         Environment.ExpandEnvironmentVariables(@"%APPDATA%\Sublime Text 3") ]
 
-  let searchPaths = if isWindows then WindowsPaths else UnixPaths
-  let directories =
-     searchPaths
-     |> List.filter Directory.Exists
+    let searchPaths = if isWindows then WindowsPaths else UnixPaths
+    let directories =
+        searchPaths
+        |> List.filter Directory.Exists
 
-  match directories.Length with
-  | 0 -> trace "No Sublime text 3 installation found"
-         exit 1
-  | _ -> directories.Head
+    match directories.Length with
+    | 0 -> trace "No Sublime text 3 installation found"
+           exit 1
+    | _ -> directories.Head
 
 let getSublimeStartArgs () =
     if not isWindows then
@@ -50,25 +50,25 @@ let getSublimeStartArgs () =
         |> Option.map (fun mo -> mo.["CommandLine"] |> string, "")
 
 let startSublime (startArgs: (string*string) option) =
-   startArgs |> Option.iter(fun (file,args) ->
-     fireAndForget (fun info ->
-       info.FileName <- file
-       info.Arguments <- args
-     )
-   )
+    startArgs |> Option.iter(fun (file,args) ->
+        fireAndForget (fun info ->
+            info.FileName <- file
+            info.Arguments <- args
+        )
+    )
 
 let killSublime () =
-   let proc = if isWindows then "sublime_text" else "Sublime Text"
-   killProcess proc
+    let proc = if isWindows then "sublime_text" else "Sublime Text"
+    killProcess proc
 
 Target "Clean" (fun _ ->
     DeleteDirs ["bin"; "release"]
 )
 
 Target "Build" (fun _ ->
-   CreateDir "bin"
-   CopyRecursive "FSharp" "bin" true |> ignore
-   Unzip "bin/fsac/fsac" "paket-files/github.com/packages/fsac"
+    CreateDir "bin"
+    CopyRecursive "FSharp" "bin" true |> ignore
+    Unzip "bin/fsac/fsac" "paket-files/github.com/packages/fsac"
 )
 
 Target "Install" (fun _ ->
