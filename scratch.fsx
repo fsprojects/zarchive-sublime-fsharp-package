e3007d978d9a9790137e0791a699563edd88f9ff
guillermooo <guillermo.lopez@outlook.com>
2015-06-21 09:40:40 +0200

    improve logging

---
+++ scratch.fsx
@@ -0,0 +1,14 @@
+open System
+
+
+let doStuff x = x + 1
+
+// this is some some some thing here that I've created! he
+let foo = "some of of this stuff is some"
+
+let stuff x = Option.iter (printfn "%A") x
+
+// this some is some
+let bar = "hey of st this"
+
+stuff (Some 10)
